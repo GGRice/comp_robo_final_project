4234e27a3b5634a5b27d32ab34cd7c55a5b164bb
sokuno222 <siena.okuno@students.olin.edu>
2018-11-27 14:21:58 -0500

    shows comments on how to set up

---
+++ hectorSLAM.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import rospy
+
+#how to launch:
+#roscore & connect to neato, teleop to move around
+#rosbag
+#roslaunch neato_2dnav hector_mapping_neato.launch
+#move around
+#rosrun map_server map_saver -f ~/mymap (saves the map)
+#end hector_mapping
+
+
+#roslaunch robot_localizer test_bagfile.launch map_name:=ac109_1 use_builtin:=true
