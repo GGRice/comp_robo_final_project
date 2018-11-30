51c534cc654bf4ad44db732d69706528572288c7
sokuno222 <siena.okuno@students.olin.edu>
2018-11-30 14:42:29 -0500

    updated cornerfinder

--- scripts/cornerfinder.py
+++ scripts/cornerfinder.py
@@ -5,36 +5,35 @@ import cv2
 import numpy as np
 
 
-def find_points(filename):
+def findPoints(filename):
     img = cv2.imread(filename)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
     gray = np.float32(gray)
     dst = cv2.cornerHarris(gray,2,3,0.04)
-
     #result is dilated for marking the corners, not important
     dst = cv2.dilate(dst,None)
-    #for showing image
+    return dst
+
+def showPoints(dst):#for showing image
     # Threshold for an optimal value, it may vary depending on the image.
     img[dst>0.01*dst.max()]=[0,0,255]
     cv2.imshow('dst',img)
     if cv2.waitKey(0) & 0xff == 27:
         cv2.destroyAllWindows()
-    return dst
-'''
-def match_points(fixed, moving):
+
+def arrangePoints(matrix):
     #finding nonzeros
-    for i in len(fixed):
-        for i in len(fixed[1]):
+    array_points = []
+    x,y = np.where(matrix > 0.01*matrix.max())
+    for i in range(0,len(x)):
+        array_points.append((x[i],y[i]))
+    return array_points
 
-    f = np.transpose(np.nonzero(fixed))
-    m = np.transpose(np.nonzero(fixed))
-    print(type(fixed[1]))
-'''
+def matchPoints(fixed, moving)
 
 
+fixed = findPoints('room1.jpg')
+fixed_array = arrangePoints(fixed)
 
-fixed = find_points('room1.jpg')
-moving = find_points('room2.jpg')
-
-#match_points(fixed, moving)
+moving = findPoints('room2.jpg')
+moving_array = arrangePoints(moving)
