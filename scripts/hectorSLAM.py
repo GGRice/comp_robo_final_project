83dfa3a7fe1f94b16317b12d757fdb067f57cfcb
GGRice <gretchrice4@gmail.com>
2018-11-30 14:33:07 -0500

    Update hectorSLAM.py

--- scripts/hectorSLAM.py
+++ scripts/hectorSLAM.py
@@ -4,7 +4,8 @@ import rospy
 
 #how to launch:
 #roscore & connect to neato, teleop to move around
-#rosbag
+#roslaunch neato_node bring_minimal.launch host:=IP_ADDRESS_OF_YOUR_ROBOT
+#rosbag record -a -x ".*map$" -o bag- file-name
 #roslaunch neato_2dnav hector_mapping_neato.launch
 #move around
 #rosrun map_server map_saver -f ~/mymap (saves the map)
