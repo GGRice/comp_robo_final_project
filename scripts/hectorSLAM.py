69b34226deaaa2408adbae5d7490f7cc4f9e693d
GGRice <gretchrice4@gmail.com>
2018-11-30 14:37:57 -0500

    Update hectorSLAM.py

--- scripts/hectorSLAM.py
+++ scripts/hectorSLAM.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 import rospy
+'''
+how to launch:
+roscore
+roslaunch neato_node bring_minimal.launch host:=IP_ADDRESS_OF_YOUR_ROBOT
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+rosbag record -a -x ".*map$" -o bag- file-name
+roslaunch neato_2dnav hector_mapping_neato.launch
+move around
+rosrun map_server map_saver -f ~/map-name (saves the map)
+end hector_mapping
 
-#how to launch:
-#roscore
-#roslaunch neato_node bring_minimal.launch host:=IP_ADDRESS_OF_YOUR_ROBOT
-#rosrun teleop_twist_keyboard teleop_twist_keyboard.py
-#rosbag record -a -x ".*map$" -o bag- file-name
-#roslaunch neato_2dnav hector_mapping_neato.launch
-#move around
-#rosrun map_server map_saver -f ~/map-name (saves the map)
-#end hector_mapping
 
-
-#roslaunch com_robo_final_project test_bagfile.launch map_name:=map-name use_builtin:=true
+roslaunch com_robo_final_project test_bagfile.launch map_name:=map-name use_builtin:=true
+'''
