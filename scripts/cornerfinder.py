5a6d669924bda31eae65eda625c4f4bb6d25321f
sokuno222 <siena.okuno@students.olin.edu>
2018-12-12 11:47:45 -0500

    updating matchpoints

--- scripts/cornerfinder.py
+++ scripts/cornerfinder.py
@@ -26,38 +26,43 @@ def showPoints(img, dst):#for showing image
 def arrangePoints(matrix):
     #finding nonzeros
     array_points = []
+    matrix[matrix<=0.01*matrix.max()]=0
     x,y = np.where(matrix > 0.01*matrix.max())
     for i in range(0,len(x)):
         array_points.append((x[i],y[i]))
-    matrix[matrix<0.01*matrix.max()]=0
     return array_points, matrix
 
 
 def matchPoints(fixed,moving,fixed_array,moving_array,a=0):#, moving):
     #TODO: run for 100 cycles
-    #make arrays the same length
-    numpoints = min((len(fixed)),(len(moving)))
-    if len(fixed)!=len(moving):
-        if len(fixed)>len(moving):
-            random.shuffle(fixed)
-            fixed=fixed[:numpoints]
-        if len(fixed)<len(moving):
-            random.shuffle(moving)
-            moving=moving[:numpoints]
-    #fixed=sorted(fixed)
-    #moving=sorted(moving)
+    #same amount of points for array and matrix
+    numpoints = min((np.count_nonzero(fixed)),(np.count_nonzero(moving)))
+    if len(fixed_array)!=len(moving_array):
+        if len(fixed_array)>len(moving_array):
+            random.shuffle(fixed_array)
+            fixed_array_bad = fixed_array[numpoints:]
+            fixed_array=fixed_array[:numpoints]
+            for i in range(0,len(fixed_array_bad)):
+                fixed[fixed_array_bad[i]] = 0
+        if len(fixed_array)<len(moving_array):
+            random.shuffle(moving_array)
+            moving_array_bad = moving_array[numpoints:]
+            moving_array=moving_array[:numpoints]
+            for i in range(0,len(moving_array_bad)):
+                moving[moving_array_bad[i]] = 0
 
+    #searching neighbors
     for i in range(0,numpoints):
-        x1,y1 = moving_array[i]
-        x2,y2 = fixed_array[i]
-        #searchingNeighbors
+
+
         X = np.square(fixed-[moving_array[i]])
-        idx = np.where( fixed_array.contains(moving_array[i]))
-        idx = idx == X.min()
-        A[idx[0], idx[1]]
-
-
+        idx = np.where(fixed_array.contains(moving_array[i]))
+        print(idx)
+        #idx = np.where(X == X.min())
+        #A[idx[0], idx[1]]
         '''
+
+
         if not (fixed_array.includes([x1,y1])):
             if not (fixed_array.includes([x1-1,y1])):
                 if not (fixed_array.includes([x1-1,y1-1])):
@@ -78,9 +83,9 @@ def matchPoints(fixed,moving,fixed_array,moving_array,a=0):#, moving):
 
 
 imgfixed,fixed = findPoints('maze1.pgm')
-showPoints(imgfixed,fixed)
+#showPoints(imgfixed,fixed)
 fixed_array, fixedzeros = arrangePoints(fixed)
-#imgmoving,moving = findPoints('room2.jpg')
-#moving_array, fixedzeros = arrangePoints(moving)
+imgmoving,moving = findPoints('room2.jpg')
+moving_array, fixedzeros = arrangePoints(moving)
 
-#matchPoints(fixed, moving,fixed_array,moving_array)
+matchPoints(fixed, moving,fixed_array,moving_array)
